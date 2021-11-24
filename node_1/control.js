8cf56ca89f42ca97e99bb97b7b22df7a9747ce92
200214 <200214rz@yse-c.net>
2021-11-24 09:28:49 +0900

    commit

---
+++ node_1/control.js
@@ -0,0 +1,4 @@
+exports.show = function(items)
+{
+    items.forEach(item=>console.log(item))
+}
---
+++ node_1/main.js
@@ -0,0 +1,4 @@
+const itemsModule = require("./items");
+const controlModule = require("./control");
+
+controlModule.show(itemsModule.items);
